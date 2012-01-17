af9a5cf8961649d119945b3481bbb195d47a25c4
fengmk2 <fengmk2@gmail.com>
2012-01-17 17:30:32 +0800

    2 dirs

--- test/ndir.test.js
+++ test/ndir.test.js
@@ -55,7 +55,7 @@ describe('ndir', function() {
         }
       });
       walker.on('end', function() {
-        dirCount.should.equal(3);
+        dirCount.should.equal(2);
         fileCount.should.equal(3);
         done();
       });
