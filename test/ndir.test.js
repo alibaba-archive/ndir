382c33502d8da8d9c609ac5a3f4c0191b6682d7a
fengmk2 <fengmk2@gmail.com>
2012-05-28 13:31:49 +0800

    add mit license; fixed code style;

--- test/ndir.test.js
+++ test/ndir.test.js
@@ -9,16 +9,16 @@ var fs = require('fs');
 var exec = require('child_process').exec;
 
 var root = path.resolve('.');
-describe('ndir', function() {
-  describe('#walk()', function() {
+describe('ndir', function () {
+  describe('#walk()', function () {
     var emptydir = path.join(root, 'test/emptydir');
 
-    before(function() {
+    before(function () {
       if (!path.existsSync(emptydir)) {
         fs.mkdirSync(emptydir);
       }
     });
-    after(function() {
+    after(function () {
       if (path.existsSync(emptydir)) {
         fs.rmdirSync(emptydir);
       }
@@ -43,82 +43,82 @@ describe('ndir', function() {
       walker.on('dir', check);
       var dirCount = 1;
       var fileCount = 0;
-      walker.on('dir', function(dirpath, files) {
+      walker.on('dir', function (dirpath, files) {
         for (var i = 0, l = files.length; i < l; i++) {
           var info = files[i];
           var stats = info[1];
           if (stats.isDirectory()) {
             dirCount++;
-          } else if(!stats.isSymbolicLink() && stats.isFile()) {
+          } else if (!stats.isSymbolicLink() && stats.isFile()) {
             fileCount++;
           }
         }
       });
-      walker.on('end', function() {
+      walker.on('end', function () {
         dirCount.should.equal(2);
         fileCount.should.equal(4);
         done();
       });
     });
 
-    it('should walk "' + root + '" in callback mode', function(done) {
+    it('should walk "' + root + '" in callback mode', function (done) {
       var walker = new dir.Walk(__dirname, check, done);
     });
 
-    it('should walk "' + root + '" no error', function(done) {
-      dir.walk(root, check, done, function(err, p) {
+    it('should walk "' + root + '" no error', function (done) {
+      dir.walk(root, check, done, function (err, p) {
         should.not.exist(err);
         should.not.exist(p);
       });
     });
 
-    it('should success when walk empty dir', function(done) {
-      dir.walk(emptydir, check, done, function(err, p) {
+    it('should success when walk empty dir', function (done) {
+      dir.walk(emptydir, check, done, function (err, p) {
         should.not.exist(err);
         should.not.exist(p);
       });
     });
 
-    it('should error when walk not exists dir', function(done) {
-      dir.walk('test/not-exists-dir', check, done, function(err) {
+    it('should error when walk not exists dir', function (done) {
+      dir.walk('test/not-exists-dir', check, done, function (err) {
         err.should.be.an.instanceof(Error);
-        err.message.should.include('ENOENT, no such file or directory');
+        err.message.should.include('ENOENT, readdir');
       });
     });
 
-    it('should error when walk a file', function(done) {
-      dir.walk('test/ndir.test.js', check, done, function(err) {
+    it('should error when walk a file', function (done) {
+      dir.walk('test/ndir.test.js', check, done, function (err) {
         err.should.be.an.instanceof(Error);
-        err.message.should.include('ENOTDIR, not a directory');
+        err.message.should.include('ENOTDIR, readdir');
       });
     });
 
     if (path.existsSync('/.fseventsd')) {
-      it('should error when walk noPermission dir', function(done) {
-        dir.walk('/.fseventsd', check, done, function(err) {
+      it('should error when walk noPermission dir', function (done) {
+        dir.walk('/.fseventsd', check, done, function (err) {
           err.should.be.an.instanceof(Error);
-          err.message.should.include('EACCES, permission denied');
+          err.message.should.include('EACCES, readdir');
         });
       });
     }
     
   });
 
-  describe('#copyfile()', function() {
+  describe('#copyfile()', function () {
     var from = 'test/dir.test.foo.txt';
     var to = 'test/dir.test.bar.txt';
     var toParentNotExists = '/tmp/' + new Date().getTime() + '/dir.test.bar.txt';
 
-    before(function() {
+    before(function () {
       path.existsSync(to) && fs.unlinkSync(to);
     });
 
-    it('should worked', function(done) {
-      dir.copyfile(from, to, function(err) {
+    it('should worked', function (done) {
+      dir.copyfile(from, to, function (err) {
         should.not.exist(err);
         fs.statSync(to).isFile().should.be.true;
         fs.readFileSync(to).toString().should.equal(fs.readFileSync(from).toString());
-        dir.copyfile(to, to, function(err) {
+        dir.copyfile(to, to, function (err) {
           // copy save should callback(err)
           err.should.be.an.instanceof(Error);
           err.message.should.include('not copied');
@@ -129,8 +129,8 @@ describe('ndir', function() {
       });
     });
 
-    it('should copy toParentNotExists', function(done) {
-      dir.copyfile(from, toParentNotExists, function(err) {
+    it('should copy toParentNotExists', function (done) {
+      dir.copyfile(from, toParentNotExists, function (err) {
         should.not.exist(err);
         fs.statSync(toParentNotExists).isFile().should.be.true;
         fs.readFileSync(toParentNotExists).toString().should.equal(fs.readFileSync(from).toString());
@@ -140,30 +140,30 @@ describe('ndir', function() {
 
   });
 
-  describe('#mkdir()', function() {
+  describe('#mkdir()', function () {
     var existsDir = '/tmp/dir.test.exists.dir';
     var notExistsDir = '/tmp/dir.test/not.exists.dir';
 
-    before(function(done) {
+    before(function (done) {
       !path.existsSync(existsDir) && fs.mkdirSync(existsDir);
       exec('rm -rf /tmp/dir.test', done);
     });
 
-    after(function() {
+    after(function () {
       fs.rmdirSync(existsDir);
     });
 
-    it('should make exists dir success', function(done) {
+    it('should make exists dir success', function (done) {
       path.existsSync(existsDir).should.be.true;
-      dir.mkdir(existsDir, function(err) {
+      dir.mkdir(existsDir, function (err) {
         path.existsSync(existsDir).should.be.true;
         done(err);
       });
     });
 
-    it('should make not exists dir success', function(done) {
+    it('should make not exists dir success', function (done) {
       path.existsSync(notExistsDir).should.be.false;
-      dir.mkdir(notExistsDir, function(err) {
+      dir.mkdir(notExistsDir, function (err) {
         path.existsSync(notExistsDir).should.be.true;
         done(err);
       });
@@ -171,13 +171,13 @@ describe('ndir', function() {
 
   });
 
-  describe('#createLineReader()', function() {
-    it('should read line by line', function(done) {
+  describe('#createLineReader()', function () {
+    it('should read line by line', function (done) {
       var logfile = __dirname + '/access.log';
       var lines = fs.readFileSync(logfile, 'utf8').split('\n');
       var index = 0;
       dir.createLineReader(logfile)
-      .on('line', function(line) {
+      .on('line', function (line) {
         line.should.be.an.instanceof(Buffer);
         var s = line.toString();
         s.should.equal(lines[index++]);
